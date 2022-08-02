4312583592be3dcb30fe559516c8fbb7a98968c2
Alex <pioner9393@gmail.com>
2022-08-03 00:17:50 +0700

    step 3

--- src/Gendiff.php
+++ src/Gendiff.php
@@ -7,5 +7,27 @@ namespace Php\Project\Lvl2\Gendiff;
 function gendiff($pathA, $pathB)
 {
     $result = '';
+    $fileA = \json_decode(\file_get_contents($pathA, true));
+    $fileB = \json_decode(\file_get_contents($pathB, true));
+
+    $keys = array_merge(get_object_vars($fileA), get_object_vars($fileB));
+    $keys = array_keys($keys);
+
+    \sort($keys);
+
+    foreach ($keys as $key) {
+        if(isset($fileA->$key) && isset($fileB->$key)) {
+            if ($fileA->$key === $fileB->$key) {
+                $result .= ('   ' . $key . ': ' . $fileA->$key . "\n");
+            } else {
+                $result .= (' - ' . $key . ': ' . $fileA->$key . "\n");
+                $result .= (' + ' . $key . ': ' . $fileB->$key . "\n");
+            }
+        } elseif (!isset($fileA->$key) && isset($fileB->$key)) {
+            $result .= (' + ' . $key . ': ' . $fileB->$key . "\n");
+        } else {
+            $result .= (' - ' . $key . ': ' . $fileA->$key . "\n");
+        }
+    }
     echo $result;
 }
---
+++ src/gendiff.js
@@ -0,0 +1,31 @@
+let j1 = {
+    "host": "hexlet.io",
+    "timeout": 50,
+    "proxy": "123.234.53.22",
+    "follow": false
+};
+
+const keys = [...Object.keys(j1), ...Object.keys(j2)];
+
+let res = '';
+
+let j2 = {
+    "timeout": 20,
+    "verbose": true,
+    "host": "hexlet.io"
+};
+
+for (let key of new Set(keys.sort())) {
+    if (j1[key] && j2[key]) {
+        if(j1[key] === j2[key]) {
+            console.log('   ', key, ': ', j1[key]);
+        } else {
+            console.log(' - ', key, ': ', j1[key]);
+            console.log(' + ', key, ': ', j2[key]);
+        }
+    } else if (!j1[key] && j2[key]) {
+        console.log(' + ', key, ': ', j2[key]);
+    } else {
+        console.log(' - ', key, ': ', j1[key]);
+    }
+};
